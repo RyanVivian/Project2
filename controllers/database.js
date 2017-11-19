4614d7085beb81ad7e5fe43da4faa64c1ad2cde0
RyanVivian <dizrascal@gmail.com>
2017-11-18 16:23:06 -0800

    1st commit - Test php and POST.

--- controllers/database.js
+++ controllers/database.js
@@ -33,6 +33,8 @@ module.exports.testPhp = function (req, res) {
     //expecting data variable called name --retrieve value using body-parser
     var body = JSON.stringify(req.body);  //if wanted entire body as JSON
     var params = JSON.stringify(req.params);//if wanted parameters
-    var first = req.body.first;  //retrieve the data associated with name
+    var first = req.body.first;  //retrieve the data associated with order data
+    var last = req.body.last;
+    var
     res.render('pages/testDisplay', {first : first});
 };
