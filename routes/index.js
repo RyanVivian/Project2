99c1303c4f376d44b226c6aaec3b5b6094509765
RyanVivian <dizrascal@gmail.com>
2017-11-18 17:30:25 -0800

    1st commit - Test php and POST.

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/mongodb', controllerDBOrders.getAllOrders);
-router.get('/testData', controllerDBOrders.testPhp);
+router.get('/testData', function (req, res) {
+
+    //now processing post
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+
+    var first = req.body.first;  //retrieve the data associated with order data
+    var last = 'last';
+    var address = 'address';
+    var city = req.body.city;
+    var state = req.body.state;
+    var zip = req.body.zip;
+    var card = req.body.card;
+    var card_num = req.body.card_num;
+    var exp_date = req.body.exp_date;
+    res.render('pages/testDisplay', {first: first, last: last, address: address});
+});
 
 
 module.exports = router;
