6aaa9ff9672f93cf98ef528a172c063fc6810b12
RyanVivian <dizrascal@gmail.com>
2017-11-21 12:51:39 -0800

    1st commit - Test php and POST.

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getAllOrders', controllerDBOrders.getAllOrders);
 router.post('/storeData', controllerDBOrders.storeData);
 
 module.exports = router;
